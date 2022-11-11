83bbad54563c3676e360e55f8b94820d09eac7f1
Wangqi-YY <117977121+Wangqi-YY@users.noreply.github.com>
2022-11-11 19:00:08 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -23,7 +23,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ohjkF5wB13kIsOiq_Z76iNtwp-qc',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: 'OvEwnHJ3WYa_aIjHe_OZnjxdFkM2Jt8dB41kyA3Yn-o',
+      useTemplateId: '	7AKOPhR_PFd_V6eHVx5_KRBof6pnAhn-2-wAIeidGiM',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '06-25',
       festivals: [
